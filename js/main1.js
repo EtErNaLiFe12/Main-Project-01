20b0dbdad724d1214d18a9209cc55c6f352b5e1d
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-06-02 21:01:22 +0900

    swiper javascript 내용 옮김

---
+++ js/main1.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 200,
+  loop: true,
+  mousewheel: true,
+  
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type : 'bullets', // 버튼 모양 결정 "bullets", "fraction" 
+    dynamicBullets: true,
+    dynamicMainBullets: 3,
+  },
+
+  autoplay : {  // 자동 슬라이드 설정 , 비 활성화 시 false
+    delay : 1100,   // 시간 설정
+    disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
+  },
+
+  //방향표
+  navigation: {
+    //다음페이지 설정
+    nextEl: '.swiper-button-next',
+    //이전페이지 설정
+    prevEl: '.swiper-button-prev',
+  },
+
+});
