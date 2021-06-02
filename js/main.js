234a86f61e56c5648fb98f1d992de96043389833
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-06-02 21:01:00 +0900

    swiper slide javascript 내용 삭제

--- js/main.js
+++ js/main.js
@@ -1,30 +1,13 @@
-const swiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  slidesPerView: 3,
-  spaceBetween: 200,
-  loop: true,
-  mousewheel: true,
-  
+const animationEl = document.querySelector('.inner .content span')
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    type : 'bullets', // 버튼 모양 결정 "bullets", "fraction" 
-    dynamicBullets: true,
-    dynamicMainBullets: 3,
-  },
+let clicked = false;
 
-  autoplay : {  // 자동 슬라이드 설정 , 비 활성화 시 false
-    delay : 1100,   // 시간 설정
-    disableOnInteraction : false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
-  },
-
-  //방향표
-  navigation: {
-    //다음페이지 설정
-    nextEl: '.swiper-button-next',
-    //이전페이지 설정
-    prevEl: '.swiper-button-prev',
-  },
-
-});
+animationEl.addEventListener ('click', function () {
+  clicked = !clicked;
+  if(clicked) {
+      console.log('log 기록 되고 있습니다.')
+      this.classList.add('clicked')
+  } else {
+        this.classList.remove('clicked')
+      }
+    }) 
