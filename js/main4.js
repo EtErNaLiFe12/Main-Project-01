c6e777a5d15a453e951c2c413d1761f74a0467d0
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-06-08 17:11:54 +0900

    gsap animation 추가

---
+++ js/main4.js
@@ -0,0 +1,567 @@
+// left
+let t1 = gsap.timeline({
+  repeat:-1,
+  // yoyo:true
+});
+t1.to('.square', {
+  x: 950,
+  duration: 0.8
+})
+t1.to('.square', {
+  y: 650,
+  duration: 0.8
+})
+t1.to('.square', {
+  x: 0,
+  duration: 0.8
+})
+t1.to('.square', {
+  y: 0,
+  duration: 0.8
+})
+
+let t2 = gsap.timeline({
+  repeat:-1,
+});
+t2.to('.circle', {
+  y: 550,
+  duration: 0.8
+})
+t2.to('.circle', {
+  x: 850,
+  duration: 0.8
+})
+t2.to('.circle', {
+  y: 0,
+  duration: 0.8
+})
+t2.to('.circle', {
+  x: 0,
+  duration: 0.8
+})
+
+let t3 = gsap.timeline({
+  repeat:-1,
+});
+t3.to('.square1', {
+  x: 750,
+  duration: 0.8
+})
+t3.to('.square1', {
+  y: 450,
+  duration: 0.8
+})
+t3.to('.square1', {
+  x: 0,
+  duration: 0.8
+})
+t3.to('.square1', {
+  y: 0,
+  duration: 0.8
+})
+
+let t4 = gsap.timeline({
+  repeat:-1,
+});
+t4.to('.circle1', {
+  y: 350,
+  duration: 0.8
+})
+t4.to('.circle1', {
+  x: 650,
+  duration: 0.8
+})
+t4.to('.circle1', {
+  y: 0,
+  duration: 0.8
+})
+t4.to('.circle1', {
+  x: 0,
+  duration: 0.8
+})
+
+let t5 = gsap.timeline({
+  repeat:-1,
+});
+t5.to('.square2', {
+  x: 550,
+  duration: 0.8
+})
+t5.to('.square2', {
+  y: 250,
+  duration: 0.8
+})
+t5.to('.square2', {
+  x: 0,
+  duration: 0.8
+})
+t5.to('.square2', {
+  y: 0,
+  duration: 0.8
+})
+
+let t6 = gsap.timeline({
+  repeat:-1,
+});
+t6.to('.circle2', {
+  y: 150,
+  duration: 0.8
+})
+t6.to('.circle2', {
+  x: 450,
+  duration: 0.8
+})
+t6.to('.circle2', {
+  y: 0,
+  duration: 0.8
+})
+t6.to('.circle2', {
+  x: 0,
+  duration: 0.8
+})
+
+let t7 = gsap.timeline({
+  repeat:-1,
+});
+t7.to('.square3', {
+  x: 350,
+  duration: 0.8
+})
+t7.to('.square3', {
+  y: 50,
+  duration: 0.8
+})
+t7.to('.square3', {
+  x: 0,
+  duration: 0.8
+})
+t7.to('.square3', {
+  y: 0,
+  duration: 0.8
+})
+
+// right
+let t8 = gsap.timeline({
+  repeat:-1,
+  // yoyo:true
+});
+t8.to('.circle3', {
+  x: -950,
+  duration: 0.8
+})
+t8.to('.circle3', {
+  y: 650,
+  duration: 0.8
+})
+t8.to('.circle3', {
+  x: 0,
+  duration: 0.8
+})
+t8.to('.circle3', {
+  y: 0,
+  duration: 0.8
+})
+
+let t9 = gsap.timeline({
+  repeat:-1,
+});
+t9.to('.square4', {
+  y: 550,
+  duration: 0.8
+})
+t9.to('.square4', {
+  x: -850,
+  duration: 0.8
+})
+t9.to('.square4', {
+  y: 0,
+  duration: 0.8
+})
+t9.to('.square4', {
+  x: 0,
+  duration: 0.8
+})
+
+let t10 = gsap.timeline({
+  repeat:-1,
+});
+t10.to('.circle4', {
+  x: -750,
+  duration: 0.8
+})
+t10.to('.circle4', {
+  y: 450,
+  duration: 0.8
+})
+t10.to('.circle4', {
+  x: 0,
+  duration: 0.8
+})
+t10.to('.circle4', {
+  y: 0,
+  duration: 0.8
+})
+
+let t11 = gsap.timeline({
+  repeat:-1,
+});
+t11.to('.square5', {
+  y: 350,
+  duration: 0.8
+})
+t11.to('.square5', {
+  x: -650,
+  duration: 0.8
+})
+t11.to('.square5', {
+  y: 0,
+  duration: 0.8
+})
+t11.to('.square5', {
+  x: 0,
+  duration: 0.8
+})
+
+let t12 = gsap.timeline({
+  repeat:-1,
+});
+t12.to('.circle5', {
+  x: -550,
+  duration: 0.8
+})
+t12.to('.circle5', {
+  y: 250,
+  duration: 0.8
+})
+t12.to('.circle5', {
+  x: 0,
+  duration: 0.8
+})
+t12.to('.circle5', {
+  y: 0,
+  duration: 0.8
+})
+
+let t13 = gsap.timeline({
+  repeat:-1,
+});
+t13.to('.square6', {
+  y: 150,
+  duration: 0.8
+})
+t13.to('.square6', {
+  x: -450,
+  duration: 0.8
+})
+t13.to('.square6', {
+  y: 0,
+  duration: 0.8
+})
+t13.to('.square6', {
+  x: 0,
+  duration: 0.8
+})
+
+let t14 = gsap.timeline({
+  repeat:-1,
+});
+t14.to('.circle6', {
+  x: -350,
+  duration: 0.8
+})
+t14.to('.circle6', {
+  y: 50,
+  duration: 0.8
+})
+t14.to('.circle6', {
+  x: 0,
+  duration: 0.8
+})
+t14.to('.circle6', {
+  y: 0,
+  duration: 0.8
+})
+
+// b-left
+let t15 = gsap.timeline({
+  repeat:-1,
+});
+t15.to('.squ', {
+  x: -950,
+  duration: 0.8
+})
+t15.to('.squ', {
+  y: -650,
+  duration: 0.8
+})
+t15.to('.squ', {
+  x: 0,
+  duration: 0.8
+})
+t15.to('.squ', {
+  y: 0,
+  duration: 0.8
+})
+
+let t16 = gsap.timeline({
+  repeat:-1,
+});
+t16.to('.cir', {
+  y: -550,
+  duration: 0.8
+})
+t16.to('.cir', {
+  x: -850,
+  duration: 0.8
+})
+t16.to('.cir', {
+  y: 0,
+  duration: 0.8
+})
+t16.to('.cir', {
+  x: 0,
+  duration: 0.8
+})
+
+let t17 = gsap.timeline({
+  repeat:-1,
+});
+t17.to('.squ1', {
+  x: -750,
+  duration: 0.8
+})
+t17.to('.squ1', {
+  y: -450,
+  duration: 0.8
+})
+t17.to('.squ1', {
+  x: 0,
+  duration: 0.8
+})
+t17.to('.squ1', {
+  y: 0,
+  duration: 0.8
+})
+
+let t18 = gsap.timeline({
+  repeat:-1,
+});
+t18.to('.cir1', {
+  y: -350,
+  duration: 0.8
+})
+t18.to('.cir1', {
+  x: -650,
+  duration: 0.8
+})
+t18.to('.cir1', {
+  y: 0,
+  duration: 0.8
+})
+t18.to('.cir1', {
+  x: 0,
+  duration: 0.8
+})
+
+let t19 = gsap.timeline({
+  repeat:-1,
+});
+t19.to('.squ2', {
+  x: -550,
+  duration: 0.8
+})
+t19.to('.squ2', {
+  y: -250,
+  duration: 0.8
+})
+t19.to('.squ2', {
+  x: 0,
+  duration: 0.8
+})
+t19.to('.squ2', {
+  y: 0,
+  duration: 0.8
+})
+
+let t20 = gsap.timeline({
+  repeat:-1,
+});
+t20.to('.cir2', {
+  y: -150,
+  duration: 0.8
+})
+t20.to('.cir2', {
+  x: -450,
+  duration: 0.8
+})
+t20.to('.cir2', {
+  y: 0,
+  duration: 0.8
+})
+t20.to('.cir2', {
+  x: 0,
+  duration: 0.8
+})
+
+let t21 = gsap.timeline({
+  repeat:-1,
+});
+t21.to('.squ3', {
+  x: -350,
+  duration: 0.8
+})
+t21.to('.squ3', {
+  y: -50,
+  duration: 0.8
+})
+t21.to('.squ3', {
+  x: 0,
+  duration: 0.8
+})
+t21.to('.squ3', {
+  y: 0,
+  duration: 0.8
+})
+
+// b-right
+
+let t22 = gsap.timeline({
+  repeat:-1,
+  // yoyo:true
+});
+t22.to('.cir3', {
+  x: 950,
+  duration: 0.8
+})
+t22.to('.cir3', {
+  y: -650,
+  duration: 0.8
+})
+t22.to('.cir3', {
+  x: 0,
+  duration: 0.8
+})
+t22.to('.cir3', {
+  y: 0,
+  duration: 0.8
+})
+
+let t23 = gsap.timeline({
+  repeat:-1,
+});
+t23.to('.squ4', {
+  y: -550,
+  duration: 0.8
+})
+t23.to('.squ4', {
+  x: 850,
+  duration: 0.8
+})
+t23.to('.squ4', {
+  y: 0,
+  duration: 0.8
+})
+t23.to('.squ4', {
+  x: 0,
+  duration: 0.8
+})
+
+let t24 = gsap.timeline({
+  repeat:-1,
+});
+t24.to('.cir4', {
+  x: 750,
+  duration: 0.8
+})
+t24.to('.cir4', {
+  y: -450,
+  duration: 0.8
+})
+t24.to('.cir4', {
+  x: 0,
+  duration: 0.8
+})
+t24.to('.cir4', {
+  y: 0,
+  duration: 0.8
+})
+
+let t25 = gsap.timeline({
+  repeat:-1,
+});
+t25.to('.squ5', {
+  y: -350,
+  duration: 0.8
+})
+t25.to('.squ5', {
+  x: 650,
+  duration: 0.8
+})
+t25.to('.squ5', {
+  y: 0,
+  duration: 0.8
+})
+t25.to('.squ5', {
+  x: 0,
+  duration: 0.8
+})
+
+let t26 = gsap.timeline({
+  repeat:-1,
+});
+t26.to('.cir5', {
+  x: 550,
+  duration: 0.8
+})
+t26.to('.cir5', {
+  y: -250,
+  duration: 0.8
+})
+t26.to('.cir5', {
+  x: 0,
+  duration: 0.8
+})
+t26.to('.cir5', {
+  y: 0,
+  duration: 0.8
+})
+
+let t27 = gsap.timeline({
+  repeat:-1,
+});
+t27.to('.squ6', {
+  y: -150,
+  duration: 0.8
+})
+t27.to('.squ6', {
+  x: 450,
+  duration: 0.8
+})
+t27.to('.squ6', {
+  y: 0,
+  duration: 0.8
+})
+t27.to('.squ6', {
+  x: 0,
+  duration: 0.8
+})
+
+let t28 = gsap.timeline({
+  repeat:-1,
+});
+t28.to('.cir6', {
+  x: 350,
+  duration: 0.8
+})
+t28.to('.cir6', {
+  y: -50,
+  duration: 0.8
+})
+t28.to('.cir6', {
+  x: 0,
+  duration: 0.8
+})
+t28.to('.cir6', {
+  y: 0,
+  duration: 0.8
+})
